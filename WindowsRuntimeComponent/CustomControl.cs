7fffc6df6a1b916812af4fea74ad6d1e9238016f
sergey <serzher@inbox.ru>
2016-12-27 17:19:32 -0800

    Create UWP Packages

---
+++ WindowsRuntimeComponent/CustomControl.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace WindowsRuntimeComponent
+{
+    public sealed class CustomControl : Control
+    {
+        public CustomControl()
+        {
+            this.DefaultStyleKey = typeof(CustomControl);
+        }
+    }
+}
