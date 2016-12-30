c262324002fc3b7cbb425b6cce342aff1cb2d767
sergey <serzher@inbox.ru>
2016-12-30 10:11:36 -0800

    load NetStandart package

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ClassLibrary.LoggerClass asd = new ClassLibrary.LoggerClass();
+        asd.Log("asd");
+
+        Console.WriteLine("Hello World!");
+    }
+}
