f873d166138df33ef33809bf4484598056903411
sergey <serzher@inbox.ru>
2017-01-20 00:48:53 -0800

    net framework package

--- AppLogger/Class1.cs
+++ AppLogger/Class1.cs
@@ -1,6 +1,19 @@
-﻿namespace AppLogger
+﻿using System;
+
+namespace AppLogger
 {
-    public class Class1
+    /// <summary>
+    /// Logger class.
+    /// </summary>
+    public class LoggerClass
     {
+        /// <summary>
+        /// Log method.
+        /// </summary>
+        /// <param name="text">Text for logging.</param>
+        public void Log(string text)
+        {
+            throw new NotImplementedException(Properties.Resources.NotImplementedException);
+        }
     }
 }
